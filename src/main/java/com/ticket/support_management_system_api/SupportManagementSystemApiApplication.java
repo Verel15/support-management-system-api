220059350eac81f4d175e23ba05dc8af6b180af5
Wichayut1503 <toei.wichayut@gmail.com>
2026-06-18 16:53:11 +0700

    Initial commit

---
+++ src/main/java/com/ticket/support_management_system_api/SupportManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.support_management_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportManagementSystemApiApplication.class, args);
+	}
+
+}
